534901154854531eaab09146bf2568268ae55338
ac.giesbrecht@gmail.com <ac.giesbrecht@gmail.com>
2020-04-23 13:32:11 -0400

    init

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBrqD5uCpHePfibhlTkSKeH1fui2ywxvDk",
+    authDomain: "chortitzer-energia.firebaseapp.com",
+    databaseURL: "https://chortitzer-energia.firebaseio.com",
+    projectId: "chortitzer-energia",
+    storageBucket: "chortitzer-energia.appspot.com",
+    messagingSenderId: "252889164504",
+    appId: "1:252889164504:web:260d50a5c251cbc2eb85ad",
+    measurementId: "G-BS80PX6Z4P"
+  }
 };
 
 /*
